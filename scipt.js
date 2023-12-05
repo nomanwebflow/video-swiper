47a6b673c0b4fb115883447a1959b901bf6f4cbb
thewebflowguy <66731092+nomanwebflow@users.noreply.github.com>
2023-12-05 18:06:54 +0500

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -12,36 +12,11 @@ const videoSwiper = new Swiper(".swiper.is-video-swiper", {
   loop: loopValue,
   grabCursor: grabCursor,
   navigation: {
-    nextEl: ".swiper-arrow.photo-swiper-next",
-    prevEl: ".swiper-arrow.photo-swiper-prev",
+    nextEl: ".swiper-arrow.swiper-next",
+    prevEl: ".swiper-arrow.swiper-prev",
   },
   keyboard: {
     enabled: keyControl,
-  },
-});
-
-const photoSwiper = new Swiper(".swiper.is-photo-swiper", {
-  speed: speed,
-  spaceBetween: 16,
-  slidesPerView: 2.5,
-  centeredSlides: true,
-  initialSlide: initialSlide,
-  loop: loopValue,
-  grabCursor: grabCursor,
-
-  breakpoints: {
-    // mobile landscape
-    480: {
-      slidesPerView: 1,
-    },
-    // tablet
-    768: {
-      slidesPerView: 2,
-    },
-    // desktop
-    992: {
-      slidesPerView: 6.5,
-    },
   },
 });
 
@@ -61,6 +36,15 @@ const contentSwiper = new Swiper(".swiper.is-content-swiper", {
   direction: "vertical",
 });
 
+const photoSwiper = new Swiper(".swiper.is-photo-swiper", {
+  speed: speed,
+  spaceBetween: 16,
+  slidesPerView: 1,
+  centeredSlides: true,
+  initialSlide: initialSlide,
+  loop: loopValue,
+  grabCursor: grabCursor,
+});
+
 videoSwiper.controller.control = [photoSwiper, headingSwiper, contentSwiper];
 photoSwiper.controller.control = [videoSwiper, headingSwiper, contentSwiper];
-
